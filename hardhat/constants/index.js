70a22cb1302da61a783c01e54ef2dd14b200775d
moe2008 <moritzdierker@gmx.de>
2022-05-16 15:48:50 +0200

    nft

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x2D0Af7Bd9Be7F7e5abFB745e44Af994CfC72dCD7";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
